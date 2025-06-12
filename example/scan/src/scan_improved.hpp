c3dd6066e40f2a1955f21a67d83eef7184046c0d
Anton Reinhard <anton.reinhard@proton.me>
2025-06-12 13:28:02 +0200

    Divorce concepts of elements per thread and number of miniblocks

--- example/scan/src/scan_improved.hpp
+++ example/scan/src/scan_improved.hpp
@@ -27,63 +27,115 @@ enum ScanType
     EXCLUSIVE_SCAN
 };
 
+constexpr IdxType operator""_idx(unsigned long long n)
+{
+    return IdxType{n};
+}
+
+template<typename TDeviceKind>
+consteval auto maximumMiniBlockSize()
+{
+    if constexpr(std::is_same_v<TDeviceKind, deviceKind::NvidiaGpu>)
+        return 8_idx;
+    else if constexpr(std::is_same_v<TDeviceKind, deviceKind::AmdGpu>)
+        return 8_idx;
+    else if constexpr(std::is_same_v<TDeviceKind, deviceKind::IntelGpu>)
+        return 8_idx;
+    else
+        return 32768_idx / sizeof(Data);
+}
+
 /* This function introduces padding to the shared memory accesses to reduce bank conflicts between threads. The
  * template parameter is the device kind, which dictates how many memory banks are assumed. For CPU or
  * unknown/unimplemented device kinds, infinite memory banks are assumed, i.e., no padding is used.
  */
 template<typename TDeviceKind>
-constexpr auto conflictFreeAccess(auto const& n)
+constexpr auto conflictFreeAccess(auto n, auto const& max)
 {
+    // TODO: do with a lambda
     if constexpr(std::is_same_v<TDeviceKind, deviceKind::NvidiaGpu>)
-        return n + n / numNvidiaBanks;
+    {
+        auto m = n + n / numNvidiaBanks;
+        if(m >= max)
+        {
+            auto nMax = max - (max / (numNvidiaBanks + 1_idx));
+            return numNvidiaBanks + (n - nMax) * (numNvidiaBanks + 1_idx);
+        }
+        else
+            return m;
+    }
     else if constexpr(std::is_same_v<TDeviceKind, deviceKind::AmdGpu>)
-        return n + n / numAmdBanks;
+    {
+        auto m = n + n / numAmdBanks;
+        if(m >= max)
+        {
+            auto nMax = max - (max / (numAmdBanks + 1_idx));
+            return numAmdBanks + (n - nMax) * (numAmdBanks + 1_idx);
+        }
+        else
+            return m;
+    }
     else if constexpr(std::is_same_v<TDeviceKind, deviceKind::IntelGpu>)
-        return n + n / numIntelBanks;
-    else // cpu or unknown backend
+    {
+        auto m = n + n / numIntelBanks;
+        if(m >= max)
+        {
+            auto nMax = max - (max / (numIntelBanks + 1_idx));
+            return numIntelBanks + (n - nMax) * (numIntelBanks + 1_idx);
+        }
+        else
+            return m;
+    }
+    else // cpu or unknown backend does nothing
         return n;
 }
 
 /* Do a muting exclusive scan on the given miniblock, and return the total sum.
  */
-ALPAKA_FN_ACC Data scanMiniBlock(auto& block, concepts::CVector auto const& extent)
+ALPAKA_FN_ACC Data scanMiniBlock(Data* block, concepts::CVector auto const& extent)
 {
+#if 0
+    std::cout << "scanMiniBlock extent: " << extent << " block data: " << block[0] << ", " << block[1] << ", ..."
+              << std::endl;
+#endif
     // -- UP-SWEEP / REDUCE --
-    for(IdxType d = extent.x() / IdxType{2}, offset = IdxType{1}; d > 0; d >>= 1, offset <<= 1)
+    for(IdxType d = extent.x() / 2_idx, offset = 1_idx; d > 0; d >>= 1, offset <<= 1)
     {
-        for(auto frameElem = IdxType{0}; frameElem < IdxType{2} * d; frameElem += IdxType{2})
+        for(auto frameElem = 0_idx; frameElem < 2_idx * d; frameElem += 2_idx)
         {
-            IdxType left = offset * (frameElem + IdxType{1}) - IdxType{1};
-            IdxType right = offset * (frameElem + IdxType{2}) - IdxType{1};
+            IdxType left = offset * (frameElem + 1_idx) - 1_idx;
+            IdxType right = offset * (frameElem + 2_idx) - 1_idx;
             block[right] += block[left];
         }
     }
 
     // save total sum
-    Data blockSum = block[extent.x() - IdxType{1}];
+    Data blockSum = block[extent.x() - 1_idx];
 
     // set 0
-    block[extent.x() - IdxType{1}] = Data{0};
+    block[extent.x() - 1_idx] = Data{0};
 
     // -- DOWN-SWEEP --
-    for(IdxType d = 1, offset = extent.x() / IdxType{2}; d < extent.x(); d <<= 1, offset >>= 1)
+    for(IdxType d = 1, offset = extent.x() / 2_idx; d < extent.x(); d <<= 1, offset >>= 1)
     {
-        for(auto frameElem = 0; frameElem < IdxType{2} * d; frameElem += IdxType{2})
+        for(auto frameElem = 0; frameElem < 2_idx * d; frameElem += 2_idx)
         {
-            IdxType left = offset * (frameElem + IdxType{1}) - IdxType{1};
-            IdxType right = offset * (frameElem + IdxType{2}) - IdxType{1};
+            IdxType left = offset * (frameElem + 1_idx) - 1_idx;
+            IdxType right = offset * (frameElem + 2_idx) - 1_idx;
             auto t = block[left];
             block[left] = block[right];
             block[right] += t;
         }
     }
-
+#if 0
+    std::cout << "returning block sum: " << blockSum << std::endl;
+#endif
     return blockSum;
 }
 
 /* Do an add increment on the given miniblock, adding the given blockSum to each element.
  */
-ALPAKA_FN_ACC void addIncrements(auto& block, Data const& blockSum, concepts::CVector auto const& extent)
+ALPAKA_FN_ACC void addIncrements(Data* block, Data const& blockSum, concepts::CVector auto const& extent)
 {
     for(auto i = 0; i < extent.x(); ++i)
     {
@@ -114,16 +166,23 @@ public:
         concepts::CVector auto chunkExtent = CVec<IdxType, elsPerThread * numThreadsPerBlock.x()>{};
         concepts::Vector auto numElements = inputVec.getExtents();
 
-        using LocalArray = Data[elsPerThread];
+        constexpr auto miniBlockSize = std::min(maximumMiniBlockSize<DeviceType>(), elsPerThread);
+        constexpr auto miniBlocksPerThread = elsPerThread / miniBlockSize;
+        constexpr auto miniBlocksPerChunk = chunkExtent.x() / miniBlockSize;
+
+        constexpr auto LocalArrayLength = miniBlocksPerThread * miniBlockSize;
+        using LocalArray = Data[LocalArrayLength];
 
 #if 0
         std::cout << "frameExtent: " << frameExtent << std::endl;
         std::cout << "numThreadsPerBlock: " << numThreadsPerBlock << std::endl;
         std::cout << "elsPerThread: " << elsPerThread << std::endl;
         std::cout << "chunkExtent: " << chunkExtent << std::endl;
+        std::cout << "miniBlockSize: " << miniBlockSize << std::endl;
+        std::cout << "miniBlocksPerThread: " << miniBlocksPerThread << std::endl;
 #endif
 
-        const auto validElementsInLastFrame = (numElements - IdxType{1}) % chunkExtent + IdxType{1};
+        const auto validElementsInLastFrame = (numElements - 1_idx) % chunkExtent + 1_idx;
 
         /* This kernel is called with 1-dimensional frame extents.
          *
@@ -134,26 +193,24 @@ public:
             onAcc::makeIdxMap(acc, onAcc::worker::blocksInGrid, IdxRange{Vec<IdxType, 1u>{0}, numFrames}))
         {
             bool const lastFrameFull = validElementsInLastFrame == chunkExtent;
-            bool const isLastFrame = frameIdx == numFrames - IdxType{1};
+            bool const isLastFrame = frameIdx == numFrames - 1_idx;
 
-            // allocate "per-thread" register memory to store mini blocks of 4 elements per thread persistently
+            // allocate "per-thread" register memory to store all mini blocks of a thread persistently
             LocalArray regMem;
 
-            auto tmp = onAcc::declareSharedMdArray<Data, uniqueId()>(
-                acc,
-                CVec<IdxType, conflictFreeAccess<DeviceType>(numThreadsPerBlock.x() - IdxType{1}) + IdxType{1}>{});
+            auto tmp = onAcc::declareSharedMdArray<Data, uniqueId()>(acc, CVec<IdxType, miniBlocksPerChunk>{});
             auto const frameOffset = chunkExtent * frameIdx;
 
-            // -- COPY TO SHARED MEM --
             for(auto frameElem : onAcc::makeIdxMap(
                     acc,
                     onAcc::worker::threadsInBlock,
                     IdxRange{CVec<IdxType, 0u>{}, chunkExtent, CVec<IdxType, elsPerThread>{}}))
             {
-                if((!lastFrameFull && isLastFrame) || elsPerThread % IdxType{4} != IdxType{0})
+                // -- COPY TO SHARED MEM --
+                if((!lastFrameFull && isLastFrame) || elsPerThread % 4_idx != 0_idx)
                 {
-                    // load into miniblock, from frameElem to frameElem + elsPerThread
-                    for(auto i = IdxType{0}; i < elsPerThread; ++i)
+                    // load into miniblocks buffer, from frameElem to frameElem + elsPerThread
+                    for(auto i = 0_idx; i < elsPerThread; ++i)
                     {
                         if(frameOffset + frameElem + i < numElements)
                             regMem[i] = inputVec[frameOffset + frameElem + i];
@@ -165,7 +222,7 @@ public:
                 {
                     MdSpanArray<LocalArray, alpaka::Alignment<16>> regMemMd{regMem};
 
-                    for(auto i = IdxType{0}; i < elsPerThread; i += IdxType{4})
+                    for(auto i = 0_idx; i < elsPerThread; i += 4_idx)
                     {
                         auto inputVecView
                             = SimdPtr{inputVec, Vec{frameOffset + frameElem + i}, Alignment<16>{}, CVec<IdxType, 4>{}};
@@ -175,26 +232,38 @@ public:
                     }
                 }
 
-                // scan miniblock
-                auto miniBlockSum = scanMiniBlock(regMem, CVec<IdxType, elsPerThread>{});
+                // -- HANDLE MINI BLOCKS OF THIS THREAD --
+                for(auto miniBlockOffset = 0_idx; miniBlockOffset < elsPerThread; miniBlockOffset += miniBlockSize)
+                {
+                    // scan miniblock
+                    /*std::cout << "regMem (1): " << regMem[0] << ", " << regMem[1] << ", " << regMem[2] << ", ..."
+                              << std::endl;*/
+                    auto miniBlockSum = scanMiniBlock(regMem + miniBlockOffset, CVec<IdxType, miniBlockSize>{});
+                    /*std::cout << "regMem (2): " << regMem[0] << ", " << regMem[1] << ", " << regMem[2] << ", ..."
+                              << std::endl;*/
 
-                // write miniblock sum into shared memory
-                tmp[conflictFreeAccess<DeviceType>(frameElem / elsPerThread)] = miniBlockSum;
+                    // write miniblock sum into shared memory
+                    tmp[conflictFreeAccess<DeviceType>(
+                        (frameElem + miniBlockOffset) / miniBlockSize,
+                        miniBlocksPerChunk)]
+                        = miniBlockSum;
+                }
             }
 
             // -- UP-SWEEP / REDUCE --
-            for(IdxType d = numThreadsPerBlock.x() / IdxType{2}, offset = IdxType{1}; d > 0; d >>= 1, offset <<= 1)
+            for(IdxType d = miniBlocksPerChunk / 2_idx, offset = 1_idx; d > 0; d >>= 1, offset <<= 1)
             {
                 onAcc::syncBlockThreads(acc);
                 for(auto frameElem : onAcc::makeIdxMap(
                         acc,
                         onAcc::worker::threadsInBlock,
-                        IdxRange{CVec<IdxType, 0>{}, Vec<IdxType, 1>{IdxType{2} * d}, IdxType{2}}))
+                        IdxRange{CVec<IdxType, 0>{}, Vec<IdxType, 1>{2_idx * d}, 2_idx}))
                 {
-                    IdxType left = offset * (frameElem + IdxType{1}).x() - IdxType{1};
-                    IdxType right = offset * (frameElem + IdxType{2}).x() - IdxType{1};
-                    left = conflictFreeAccess<DeviceType>(left);
-                    right = conflictFreeAccess<DeviceType>(right);
+                    IdxType left = offset * (frameElem + 1_idx).x() - 1_idx;
+                    IdxType right = offset * (frameElem + 2_idx).x() - 1_idx;
+                    left = conflictFreeAccess<DeviceType>(left, miniBlocksPerChunk);
+                    right = conflictFreeAccess<DeviceType>(right, miniBlocksPerChunk);
+                    // std::cout << "up-sweep " << left << ", " << right << std::endl;
                     tmp[right] += tmp[left];
                 }
             }
@@ -206,27 +275,28 @@ public:
                 if constexpr(sizeof...(blockSums))
                 {
                     auto _blockSums = std::get<0>(std::make_tuple(blockSums...));
-                    _blockSums[frameIdx] = tmp[conflictFreeAccess<DeviceType>(numThreadsPerBlock - IdxType{1})];
+                    _blockSums[frameIdx]
+                        = tmp[conflictFreeAccess<DeviceType>(miniBlocksPerChunk - 1_idx, miniBlocksPerChunk)];
                 }
 
                 // -- SET 0 --
-                tmp[conflictFreeAccess<DeviceType>(numThreadsPerBlock - IdxType{1})] = 0;
+                tmp[conflictFreeAccess<DeviceType>(miniBlocksPerChunk - 1_idx, miniBlocksPerChunk)] = 0;
             }
 
             // -- DOWN-SWEEP --
-            for(IdxType d = 1, offset = numThreadsPerBlock.x() / IdxType{2}; d < numThreadsPerBlock;
-                d <<= 1, offset >>= 1)
+            for(IdxType d = 1, offset = miniBlocksPerChunk / 2_idx; d < miniBlocksPerChunk; d <<= 1, offset >>= 1)
             {
                 onAcc::syncBlockThreads(acc);
                 for(auto frameElem : onAcc::makeIdxMap(
                         acc,
                         onAcc::worker::threadsInBlock,
-                        IdxRange{CVec<IdxType, 0>{}, Vec<IdxType, 1>{IdxType{2} * d}, IdxType{2}}))
+                        IdxRange{CVec<IdxType, 0>{}, Vec<IdxType, 1>{2_idx * d}, 2_idx}))
                 {
-                    IdxType left = offset * (frameElem.x() + IdxType{1}) - IdxType{1};
-                    IdxType right = offset * (frameElem.x() + IdxType{2}) - IdxType{1};
-                    left = conflictFreeAccess<DeviceType>(left);
-                    right = conflictFreeAccess<DeviceType>(right);
+                    IdxType left = offset * (frameElem.x() + 1_idx) - 1_idx;
+                    IdxType right = offset * (frameElem.x() + 2_idx) - 1_idx;
+                    left = conflictFreeAccess<DeviceType>(left, miniBlocksPerChunk);
+                    right = conflictFreeAccess<DeviceType>(right, miniBlocksPerChunk);
+                    // std::cout << "down-sweep " << left << ", " << right << std::endl;
                     auto t = tmp[left];
                     tmp[left] = tmp[right];
                     tmp[right] += t;
@@ -240,20 +310,31 @@ public:
                     onAcc::worker::threadsInBlock,
                     IdxRange{CVec<IdxType, 0u>{}, chunkExtent, CVec<IdxType, elsPerThread>{}}))
             {
-                // load block sum from shared memory
-                Data blockSum;
-                if(frameOffset + frameElem < numElements)
+                // -- HANDLE MINI BLOCKS OF THIS THREAD --
+                for(auto miniBlockOffset = 0_idx; miniBlockOffset < elsPerThread; miniBlockOffset += miniBlockSize)
                 {
-                    blockSum = tmp[conflictFreeAccess<DeviceType>(frameElem / elsPerThread)];
+                    // load block sum from shared memory
+                    Data blockSum;
+                    if(frameOffset + frameElem + miniBlockOffset < numElements)
+                    {
+                        blockSum = tmp[conflictFreeAccess<DeviceType>(
+                            (frameElem + miniBlockOffset) / miniBlockSize,
+                            miniBlocksPerChunk)];
+                        // std::cout << "reading block sum " << blockSum << std::endl;
+                    }
+
+                    /*std::cout << "regMem (3): " << regMem[0] << ", " << regMem[1] << ", " << regMem[2] << ", ..."
+                              << std::endl;*/
+                    // add block sum to mini block
+                    addIncrements(regMem + miniBlockOffset, blockSum, CVec<IdxType, miniBlockSize>{});
+                    /*std::cout << "regMem (4): " << regMem[0] << ", " << regMem[1] << ", " << regMem[2] << ", ..."
+                              << std::endl;*/
                 }
 
-                // add block sum to mini block
-                addIncrements(regMem, blockSum, CVec<IdxType, elsPerThread>{});
-
-                if((!lastFrameFull && isLastFrame) || elsPerThread % IdxType{4} != IdxType{0})
+                if((!lastFrameFull && isLastFrame) || elsPerThread % 4_idx != 0_idx)
                 {
                     // write back to global mem, from frameElem to frameElem + elsPerThread
-                    for(auto i = IdxType{0}; i < elsPerThread; ++i)
+                    for(auto i = 0_idx; i < elsPerThread; ++i)
                     {
                         if(frameOffset + frameElem + i < numElements)
                             outputVec[frameOffset + frameElem + i] = regMem[i];
@@ -263,7 +344,7 @@ public:
                 {
                     MdSpanArray<LocalArray, alpaka::Alignment<16>> regMemMd{regMem};
 
-                    for(auto i = IdxType{0}; i < elsPerThread; i += IdxType{4})
+                    for(auto i = 0_idx; i < elsPerThread; i += 4_idx)
                     {
                         auto outputVecView = SimdPtr{
                             outputVec,
@@ -340,7 +421,7 @@ void exclusiveScan(auto& exec, auto& devAcc, auto& queue, auto const& inputVec, 
     auto numFrames = divCeil(inputVec.getExtents(), chunkExtent);
     auto const frameSpec = onHost::FrameSpec{numFrames, chunkExtent, CVec<IdxType, 256u>{}};
 
-    if(frameSpec.m_numFrames > IdxType{1})
+    if(frameSpec.m_numFrames > 1_idx)
     {
         // problem does not fit in 1 frame, recurse
         ExclusiveScan_AddIncrementsKernel addIncrements;
