7486c13e5ef70963982576834b9aeaecec2e151f
René Widera <r.widera@hzdr.de>
2025-08-27 11:38:10 +0200

    `alpaka-ls` add windows dynamic linking support
    The windows dynamic linking is written blindly without testing it,
    because we do not have windows systems.

--- example/alpaka-ls/src/ls.cpp
+++ example/alpaka-ls/src/ls.cpp
@@ -6,7 +6,12 @@
 #include <alpaka/example/executeForEach.hpp>
 #include <alpaka/example/executors.hpp>
 
-#include <dlfcn.h>
+#if ALPAKA_OS_WINDOWS
+#    include <direct.h>
+#    include <windows.h>
+#else
+#    include <dlfcn.h>
+#endif
 
 #include <algorithm>
 #include <filesystem>
@@ -28,34 +33,64 @@ int example(int argc, char* argv[], auto const usedApi)
 {
     std::filesystem::path p = argv[0];
     auto binaryPath = p.parent_path().string();
-    void* handle = nullptr;
 
-    auto prefixList = std::vector<std::string>{".", binaryPath};
+#if ALPAKA_OS_WINDOWS
+    HINSTANCE handle;
+#else
+    void* handle = nullptr;
+#endif
+
+    // try to load first in working directory, then in binary directory, then via LD_LIBRARY_PATH
+    auto prefixList = std::vector<std::string>{"./", binaryPath + "/", ""};
     for(auto libPrefix : prefixList)
     {
-        auto libraryName = libPrefix + "/libalpaka-ls_" + onHost::getName(usedApi) + ".so";
-        handle = dlopen(toLower(libraryName).c_str(), RTLD_LAZY);
+        auto libraryName = libPrefix + "libalpaka-ls_" + onHost::getName(usedApi);
+
+#if ALPAKA_OS_WINDOWS
+        libraryName += ".dll";
+        handle = LoadLibrary(TEXT((libraryName).c_str()));
+#else
+#    if ALPAKA_OS_IOS
+        libraryName += ".dylib";
+#    else
+        libraryName += ".so";
+#    endif
+
+        handle = dlopen(toLower(libraryName).c_str(), RTLD_NOW | RTLD_LAZY | RTLD_GLOBAL);
+#endif
         if(handle)
             break;
     }
     if(handle)
     {
         // Lookup function
+#if ALPAKA_OS_WINDOWS
+        fn_startBackend = (startBackend_t) GetProcAddress(libBackend, "xmrstak_start_backend");
+        if(!func)
+        {
+            std::cerr << "dlsym failed: " << GetLastError() << "\n";
+            FreeLibrary(handle);
+        }
+#else
         auto func = reinterpret_cast<alpaka_main_t*>(
             dlsym(handle, "alpaka_main") // <-- if compiled without extern "C", use mangled
         );
-
         if(!func)
         {
             std::cerr << "dlsym failed: " << dlerror() << "\n";
             dlclose(handle);
         }
+#endif
 
         if(func)
         {
             // Forward argc/argv from main to alpaka_main
             [[maybe_unused]] int ret = func(argc, argv);
+#if ALPAKA_OS_WINDOWS
+            FreeLibrary(handle);
+#else
             dlclose(handle);
+#endif
         }
     }
 
